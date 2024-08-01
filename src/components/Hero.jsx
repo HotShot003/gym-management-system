905467adfc87458705646c629d7c3093e193fa89
Anurag Padhiyar <121120034+HotShot003@users.noreply.github.com>
2024-08-02 00:14:10 +0530

    Feats : Added Hero Comopents

---
+++ src/components/Hero.jsx
@@ -0,0 +1,26 @@
+// eslint-disable-next-line no-unused-vars
+import React from "react";
+import './Hero.css'
+const Hero = () => {
+
+    return(
+        <section className="hero">
+            <div className="content">
+                <div className="title">
+                <h1>Empower</h1>
+                <h1>Your</h1>
+                <h1>Fitness</h1> 
+                </div>
+                <div className="sub-title">
+                    <p>Commit. Conquer. Celebrate Your Progress.</p>          <p>Unleash Your Potential</p>
+                </div>
+                <div className="buttons">
+                    <button>Embark on Your Journey</button>
+                    <button>Check Out HOT Offers</button>
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default Hero;
